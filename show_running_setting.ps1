f95a5ab8d1df04347884bb21430d9098ab03db57
Mahbubi Hamdani <47600643+mahbubi@users.noreply.github.com>
2022-06-28 11:09:05 +0700

    Create show_running_setting.ps1

---
+++ show_running_setting.ps1
@@ -0,0 +1,14 @@
+$GroupName = "(Security_Group)"
+Connect-AzureAD
+
+$settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+if(!$settingsObjectID)
+{
+    $template = Get-AzureADDirectorySettingTemplate | Where-object {$_.displayname -eq "group.unified"}
+    $settingsCopy = $template.CreateDirectorySetting()
+    New-AzureADDirectorySetting -DirectorySetting $settingsCopy
+    $settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+}
+
+$settingsCopy = Get-AzureADDirectorySetting -Id $settingsObjectID
+(Get-AzureADDirectorySetting -Id $settingsObjectID).Values
